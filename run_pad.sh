d86759b7fecb78a7f40370a47e72a0668d7e85a2
Bryant Hansen <bryant.hansen@hslu.ch>
2024-04-28 20:31:36 +0200

    last tweaks to make the training run

--- run_pad.sh
+++ run_pad.sh
@@ -10,15 +10,13 @@ time python pad_experiments.py \
       --weighted_loss \
       --root_path_coco coco_files/ \
       --prefix_coco 20240417101117 \
-      --netcdf_path ../dataset/netcdf/ \
+      --netcdf_path ../dataset/ \
       --prefix 20240417101117 \
       --num_epochs 10 \
       --batch_size 32 \
       --bands B02 B03 B04 B08 \
-      --saved_medians \
       --img_size 61 61 \
       --requires_norm \
-      --num_workers 16 \
+      --num_workers 8 \
       --num_gpus 1 \
       --fixed_window \
-      --saved_medians
