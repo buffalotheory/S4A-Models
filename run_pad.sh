df40aa96ee594f9c5f7edc848c6f7efca5e33817
Bryant Hansen <bryant.hansen@hslu.ch>
2024-05-03 15:38:36 +0200

    next test

--- run_pad.sh
+++ run_pad.sh
@@ -9,14 +9,15 @@ time python pad_experiments.py \
       --parcel_loss \
       --weighted_loss \
       --root_path_coco coco_files/ \
-      --prefix_coco 20240503132753 \
+      --prefix_coco 20240503153528 \
       --netcdf_path ./dataset/ \
-      --prefix 20240503132753 \
-      --num_epochs 10 \
-      --batch_size 32 \
+      --prefix 20240503153528 \
+      --num_epochs 5 \
+      --batch_size 8 \
       --bands B02 B03 B04 B08 \
       --img_size 61 61 \
       --requires_norm \
       --num_workers 8 \
       --num_gpus 1 \
       --fixed_window \
+      --saved_medians=True
