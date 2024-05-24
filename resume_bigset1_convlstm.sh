d853e01412542583f7c3bb3bafd844e9cb35806f
Bryant Hansen <bryant.hansen@hslu.ch>
2024-05-25 00:44:18 +0200

    minor test updates

--- resume_bigset1_convlstm.sh
+++ resume_bigset1_convlstm.sh
@@ -35,19 +35,12 @@ time python pad_experiments.py \
       --num_workers $NUM_WORKERS \
       --num_gpus 1 \
       --fixed_window \
+      --wandb \
 
 ECODE=$?
 set +x
 echo "[$(date "+%Y-%m-%d %H:%M:%S")]:INFO:${0}:pad_experiments.py resume (${MODEL} / ${PREFIX}) exited with ECODE $ECODE" >&2
 exit $ECODE
 
-# Disabled options
-#      --wandb \
-#
-
-# Previous checkpoint examples
+# Checkpoint can be specified by name
 #ckpt=/Users/iahansen/admin/master_study/P7/repo/Sen4AgriNet/S4A-Models/logs/convlstm/20240516144730/run_20240517075542/checkpoints/epoch=0-step=4.ckpt
-#ckpt=/Users/iahansen/admin/master_study/P7/repo/Sen4AgriNet/S4A-Models/logs/convlstm/202405170804/run_20240517090313/checkpoints/epoch=4-step=45.ckpt
-#ckpt=/Users/iahansen/admin/master_study/P7/repo/Sen4AgriNet/S4A-Models/logs/convlstm/20240517_095529/run_20240517095605/checkpoints/epoch=4-#step=45.ckpt
-#ckpt=/Users/iahansen/admin/master_study/P7/repo/Sen4AgriNet/S4A-Models/logs/convlstm/20240520/run_20240520193115/checkpoints/epoch=10-step=44.ckpt
-#ckpt=/Users/iahansen/admin/master_study/P7/repo/Sen4AgriNet/S4A-Models/logs/convlstm/20240522/run_20240522113233/checkpoints/epoch=4-step=20.ckpt
