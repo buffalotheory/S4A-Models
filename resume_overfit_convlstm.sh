7433aaf702e987cbf627158bde1575ce5de93c06
Bryant Hansen <bryant.hansen@students.fhnw.ch>
2024-05-31 10:03:56 +0200

    update experiment

--- resume_overfit_convlstm.sh
+++ resume_overfit_convlstm.sh
@@ -3,13 +3,11 @@
 
 set -x
 
-# Variables to offer at this stage of the test run
-
 MODEL=convlstm
-EPOCHS=5
+EPOCHS=10
 PREFIX=overfit
-BATCH_SIZE=9
-NUM_WORKERS=9
+BATCH_SIZE=4
+NUM_WORKERS=16
 
 [[ "$1" == '-e' ]] && EPOCHS=$2
 
@@ -19,15 +17,16 @@ echo "[$(date "+%Y-%m-%d %H:%M:%S")]:INFO:${0}:resuming pad_experiments.py with 
 
 ckpt=last
 
-time python pad_experiments.py \
+cd /app/S4A-Models \
+&& time python pad_experiments.py \
       --train \
       --resume $ckpt \
       --model $MODEL \
       --parcel_loss \
       --weighted_loss \
-      --root_path_coco coco_files/ \
+      --root_path_coco /app/S4A-Models/coco_files/ \
       --prefix_coco $PREFIX \
-      --netcdf_path ../dataset/ \
+      --netcdf_path /app/dataset/ \
       --prefix ${results_path} \
       --num_epochs $EPOCHS \
       --batch_size $BATCH_SIZE \
@@ -35,21 +34,11 @@ time python pad_experiments.py \
       --img_size 61 61 \
       --requires_norm \
       --num_workers $NUM_WORKERS \
-      --num_gpus 1 \
+      --num_gpus 2 \
       --fixed_window \
+      --wandb \
 
 ECODE=$?
 set +x
 echo "[$(date "+%Y-%m-%d %H:%M:%S")]:INFO:${0}:pad_experiments.py resume (${MODEL} / ${PREFIX}) exited with ECODE $ECODE" >&2
 exit $ECODE
-
-# Disabled options
-#      --wandb \
-#
-
-# Previous checkpoint examples
-#ckpt=/Users/iahansen/admin/master_study/P7/repo/Sen4AgriNet/S4A-Models/logs/convlstm/20240516144730/run_20240517075542/checkpoints/epoch=0-step=4.ckpt
-#ckpt=/Users/iahansen/admin/master_study/P7/repo/Sen4AgriNet/S4A-Models/logs/convlstm/202405170804/run_20240517090313/checkpoints/epoch=4-step=45.ckpt
-#ckpt=/Users/iahansen/admin/master_study/P7/repo/Sen4AgriNet/S4A-Models/logs/convlstm/20240517_095529/run_20240517095605/checkpoints/epoch=4-#step=45.ckpt
-#ckpt=/Users/iahansen/admin/master_study/P7/repo/Sen4AgriNet/S4A-Models/logs/convlstm/20240520/run_20240520193115/checkpoints/epoch=10-step=44.ckpt
-#ckpt=/Users/iahansen/admin/master_study/P7/repo/Sen4AgriNet/S4A-Models/logs/convlstm/20240522/run_20240522113233/checkpoints/epoch=4-step=20.ckpt
