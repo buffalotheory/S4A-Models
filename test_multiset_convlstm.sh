70c9abcf3301a5776de6fdf778eaa450b22cfa0a
Bryant Hansen <bryant.hansen@students.fhnw.ch>
2024-06-01 14:55:50 +0200

    update multiset test

--- test_multiset_convlstm.sh
+++ test_multiset_convlstm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# from
+# Bryant Hansen
+
+# Execute the test of convLSTM with the multiset dataset
 
 set -x
 
@@ -8,41 +10,45 @@ EPOCHS=5
 PREFIX=multiset
 BATCH_SIZE=9
 NUM_WORKERS=9
+GPUS=1
 
-results_path=${PREFIX}_bs${BATCH_SIZE}
+RESULTS_PATH=${PREFIX}
+MODEL_PATH=/app/S4A-Models/
+DATASET_PATH=/app/dataset/
+COCO_PATH=${MODEL_PATH}/coco_files/
+CONF_DIR=${MODEL_PATH}/conf/
+
+ME=$(basename $0)
+CONF=${ME%.sh}.conf
+[[ ! -f "$CONF_DIR"/s4a.conf ]] || . "$CONF_DIR"/s4a.conf
+[[ ! -f "$CONF_DIR"/"$CONF" ]] || . "$CONF_DIR"/"$CONF"
+[[ ! -f "$CONF" ]]             || . "$CONF"
 
 echo "[$(date "+%Y-%m-%d %H:%M:%S")]:INFO:${0}:starting pad_experiments.py with model ${MODEL}.  logging to ${results_path}" >&2
 
+last_checkpoint_file="$(ls -1t ${MODEL_PATH}/logs/convlstm/overfit/run_*/checkpoints/*.ckpt | head -n 1)"
+echo "last_checkpoint_file: $last_checkpoint_file" >&2
+
+cd $MODEL_PATH \
+&& time python pad_experiments.py \
 time python pad_experiments.py \
       --model $MODEL \
       --parcel_loss \
       --weighted_loss \
-      --root_path_coco coco_files/ \
+      --root_path_coco $COCO_PATH \
       --prefix_coco $PREFIX \
-      --netcdf_path ../dataset/ \
-      --prefix ${results_path} \
+      --netcdf_path $DATASET_PATH \
+      --prefix $RESULTS_PATH \
       --num_epochs $EPOCHS \
       --batch_size $BATCH_SIZE \
       --bands B02 B03 B04 B08 \
       --img_size 61 61 \
       --requires_norm \
       --num_workers $NUM_WORKERS \
-      --num_gpus 1 \
+      --num_gpus $GPUS \
       --fixed_window \
-      --load_checkpoint=logs/convlstm/multiset_bs9/run_20240524094301/checkpoints/epoch=4-step=40-v2.ckpt
+      --load_checkpoint "$last_checkpoint_file"
 
 ECODE=$?
 set +x
 echo "[$(date "+%Y-%m-%d %H:%M:%S")]:INFO:${0}:pad_experiments.py run (${MODEL} / ${PREFIX}) exited with ECODE $ECODE" >&2
-
-
-#      --prefix $(date "+%Y%m%d_%H%M%S") \
-#      --wandb \
-#      --saved_medians \
-
-
-      #--window_len 12
-
-
-      # \
-      #--saved_medians
