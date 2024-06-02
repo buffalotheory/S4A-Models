1d7606a35a6891cb5823756992d31d2fec53dab6
Bryant Hansen <bryant.hansen@students.fhnw.ch>
2024-06-02 10:27:41 +0200

    update log messages

--- model/PAD_convLSTM.py
+++ model/PAD_convLSTM.py
@@ -494,7 +494,7 @@ class ConvLSTM(pl.LightningModule):
             #pred_disc = bins_idx - 1
 
         logging.info(f'batch_idx: {batch_idx}: inputs {inputs.shape}, labels: {label.shape}, preds: {pred.shape}, conf_mat: {self.confusion_matrix.shape}')
-        logging.info(f'batch_idx: {batch_idx}: label.shape[0]: {label.shape[0]}')
+        logging.info(f'batch_idx: {batch_idx}: label.shape[0]: {label.shape[0]}; unique labels in 0: {np.unique(label[0])}')
 
         for i in range(label.shape[0]):
             self.confusion_matrix[label[i], pred[i]] += 1
