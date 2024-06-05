d9aee07d98180e394eca4ae145d8db27d25a95b7
Bryant Hansen <bryant.hansen@hslu.ch>
2024-06-05 23:01:00 +0200

    new 100-patch training set and apparatus

---
+++ run_multiset_100_convlstm.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Bryant Hansen
+
+# Launch the convLSTM test with the multiset dataset
+
+set -x
+
+MODEL=convlstm
+EPOCHS=20
+PREFIX=dataset_100
+BATCH_SIZE=4
+NUM_WORKERS=8
+GPUS=2
+
+[[ "$1" == '-e' ]] && EPOCHS=$2
+
+RESULTS_PATH=${PREFIX}
+MODEL_PATH=.
+DATASET_PATH=../dataset_100/
+COCO_PATH=${MODEL_PATH}/coco_files/
+CONF_DIR=${MODEL_PATH}/conf/
+
+ME=$(basename $0)
+MYCONF=${ME%.sh}.conf
+[[ ! -f "$CONF_DIR"/s4a.conf ]]  || . "$CONF_DIR"/s4a.conf
+[[ ! -f "$CONF_DIR"/"$MYCONF" ]] || . "$CONF_DIR"/"$MYCONF"
+
+echo "[$(date "+%Y-%m-%d %H:%M:%S")]:INFO:${0}:starting pad_experiments.py with model ${MODEL}.  logging to ${RESULTS_PATH}" >&2
+
+cd $MODEL_PATH \
+&& time python pad_experiments.py \
+      --train \
+      --model $MODEL \
+      --parcel_loss \
+      --weighted_loss \
+      --root_path_coco $COCO_PATH \
+      --prefix_coco $PREFIX \
+      --netcdf_path $DATASET_PATH \
+      --prefix $RESULTS_PATH \
+      --num_epochs $EPOCHS \
+      --batch_size $BATCH_SIZE \
+      --bands B02 B03 B04 B08 \
+      --img_size 61 61 \
+      --requires_norm \
+      --num_workers $NUM_WORKERS \
+      --num_gpus $GPUS \
+      --fixed_window \
+
+#      --wandb \
+
+ECODE=$?
+set +x
+echo "[$(date "+%Y-%m-%d %H:%M:%S")]:INFO:${0}:pad_experiments.py run (${MODEL} / ${PREFIX}) exited with ECODE $ECODE" >&2
+exit $ECODE
